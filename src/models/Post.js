3d6154bd1cb9513338cbc21219abcfa60bad5312
lu15v <lu15bytes@gmail.com>
2021-01-25 23:44:24 -0600

    CREATED: models post and user

---
+++ src/models/Post.js
@@ -0,0 +1,46 @@
+const {model, Schema} = require('mongoose');
+
+
+const postSchema = new Schema({
+    body: {
+        type: String, 
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    comments: [
+        {
+            body: String,
+            username: String,
+            createdAt: {
+                type: Date,
+                default: Date.now
+            },
+
+        }
+    ],
+    likes: [
+        {
+            username: String,
+            createdAt: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'Users'
+    }
+
+});
+
+
+
+module.exports = model('Post', postSchema);
