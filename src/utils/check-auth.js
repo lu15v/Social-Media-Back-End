9b87ad581c2e2e46de8385eedf315643fac9be8b
lu15v <lu15bytes@gmail.com>
2021-02-01 01:05:40 -0600

    FEATURE: auth function created

---
+++ src/utils/check-auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const {SECRET_KEY} = require('../../config.js');
+const {AuthenticationError} = require('apollo-server');
+
+
+module.exports = (context) => {
+    const authHeader = context.req.headers.authorization;
+
+    if(authHeader){
+        //Bearer.. convention used
+        const token = authHeader.split('Bearer ')[1];
+        if(token){
+            try{
+                const user = jwt.verify(token, SECRET_KEY);
+                return user;
+            }catch{
+                throw new AuthenticationError('Invalid/Expired token');
+            }
+        }
+        throw new Error('token invalid or wrong format, it must be beared');
+    }
+    throw new Error('Auth header must be provided');
+}
