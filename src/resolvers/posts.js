43ba03c1fa03790c53645c317d584a5b18ed0dca
lu15v <lu15bytes@gmail.com>
2021-02-03 00:43:36 -0600

    FEATURE: post resolvers added (likePost) and likeCount, commentCount

--- src/resolvers/posts.js
+++ src/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require('../models/Post');
 const checkAuth = require('../utils/check-auth');
-const {AuthenticationError} = require('apollo-server');
+const {AuthenticationError, UserInputError} = require('apollo-server');
 
 module.exports = {
     Query:{
@@ -61,6 +61,30 @@ module.exports = {
              }catch(err){
                  throw new Error(err);
              }
+         },
+         async likePost(_, {postId}, context){
+            const {username} = checkAuth(context);
+
+            try{
+                const post = await Post.findById(postId);
+                
+                if(post){
+
+                    if(post.likes.find(like => like.username === username)){
+                        //Post already liked, unliked.
+                        post.likes = post.likes.filter(like => like.username !== username);
+                    }else{
+                        post.likes.push({
+                            username
+                        })
+                    }
+                    await post.save();
+                    return post;
+                }
+                throw new UserInputError("Post not found");
+            }catch(err){
+                throw new Error(err);
+            }
          }
      }
 }
